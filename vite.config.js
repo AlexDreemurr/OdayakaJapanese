a14d421c7fe4de0cad06d0c675ceb9b37e2964a1
AlexDreemurr <stanleyatcheson187@gmail.com>
2026-05-25 23:35:46 +0800

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
-const base = process.env.VITE_BASE_PATH || "/";
+const base = process.env.VERCEL ? "/" : process.env.VITE_BASE_PATH || "/";
 
 // https://vite.dev/config/
 export default defineConfig({
